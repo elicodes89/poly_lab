f550cb02f5e29ffab2e1050adad5fb390a2f8979
elicodes89 <elicodes89@gmail.com>
2022-05-16 14:33:32 +0100

    extension finished. test passing

--- polymorphism_lab/polymorphism_lab_start/src/main/java/CreditCard.java
+++ polymorphism_lab/polymorphism_lab_start/src/main/java/CreditCard.java
@@ -1,25 +1,15 @@
-public class CreditCard implements IScan{
+public class CreditCard extends PaymentCard implements IScan{
     private String cardNumber;
     private String expiryDate;
     private int securityNumber;
 
     public CreditCard(String cardNumber, String expiryDate, int securityNumber) {
-        this.cardNumber = cardNumber;
-        this.expiryDate = expiryDate;
-        this.securityNumber = securityNumber;
+        super(cardNumber, expiryDate, securityNumber);
+//        this.cardNumber = cardNumber;
+//        this.expiryDate = expiryDate;
+//        this.securityNumber = securityNumber;
     }
 
-    public String getCardNumber() {
-        return this.cardNumber;
-    }
-
-    public String getExpiryDate() {
-        return this.expiryDate;
-    }
-
-    public int getSecurityNumber() {
-        return this.securityNumber;
-    }
 
     public String scan() {
         return "Payment Successful";
